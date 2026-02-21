b253a1738b9b9405e54084253172514bc5c6f6c6
Klinti Bodurri <klintibodurri@Klintis-MacBook-Air.local>
2026-02-21 01:11:46 +0100

    “update”

--- models/User.js
+++ models/User.js
@@ -8,15 +8,17 @@ const userSchema = new mongoose.Schema({
     isAdmin: { type: Boolean, required: true, default: false },
     points: { type: Number, default: 0 },
     
-    // 👇 SHTUAR TANI PËR SISTEMIN AUTOFILL TË ADRESËS
+    // Fushat e adresës
     address: { type: String, default: '' },
     city: { type: String, default: 'Elbasan' },
     phone: { type: String, default: '' }
-
 }, { timestamps: true });
 
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) next();
+    // MAGJIA KËTU: Fjala 'return' e ndalon serverin të prishë fjalëkalimin!
+    if (!this.isModified('password')) {
+        return next(); 
+    }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
